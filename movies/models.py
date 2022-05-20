17b70acee00e35e651c9481824338f4ef52df33a
Feyza Zeynep Kılıç <fzeynep.kilic@igdir.edu.tr>
2022-05-20 09:49:47 +0300

    updated

---
+++ movies/models.py
@@ -0,0 +1,13 @@
+from math import fabs
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Movie(models.Model):
+    film_adi = models.CharField(max_length=200)
+    film_aciklama = models.TextField()
+    resim = models.CharField(max_length=100)
+    anasayfa = models.BooleanField(default=False)
